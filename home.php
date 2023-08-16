1e8cddd8fa92ae7d4fbf17be29fd8f5f39d42d82
danilo-andjelic <daniloandjelici9@gmail.com>
2023-08-16 18:03:54 +0200

    First draft

---
+++ home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bioskop D</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
+<body style="background-color: lightblue">
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+            <a class="navbar-brand" href="#">Bioskop D</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="home.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Filmovi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Autori</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">O nama</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                           data-bs-toggle="dropdown" aria-expanded="false">
+                            Dodaj
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Novi film</a></li>
+                            <li><a class="dropdown-item" href="#">Novog Autora</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <main class="container mt-5" style="background-color: lightblue">
+        <h1 class="display-4">Bioskop D</h1>
+        <p class="lead">Welcome to Bioskop D, your ultimate movie theater experience.</p>
+        <p>Kontakt: da20190243@student.fon.bg.ac.rs</p>
+    </main>    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
